6678166a3cc62daa8b19f52f56494426f056b00e
Soumyajit <soumyajit40cs@gmail.com>
2018-09-24 13:49:07 +0530

    Update admin.py
    custom admin action implementation

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,27 @@
 from django.contrib import admin
-from .models import Post, Comment, Contact, Category, Subcategory
+from .models import Post, Comment, Contact, Continent, Country
+from django.utils import timezone
 # Register your models here.
 
 
+
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','author','created_date',)
+    list_display = ('title','author','created_date','published_date',)
     search_fields = ['title']
     ordering = ['-created_date']
+    actions = ['make_published',]
     #list_filter = ['pub_date']
     #date_hierarchy = 'pub_date'
+    def make_published(modeladmin, request, queryset):
+        for post in queryset:
+            post.published_date = timezone.now()
+            post.save()
+    make_published.short_description = "Mark selected stories as published"
 
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Contact)
-admin.site.register(Category)
-admin.site.register(Subcategory)
+admin.site.register(Continent)
+admin.site.register(Country)
